4722bf7f86e08339b3dcfc2dbf698b4511eb5581
gruand <gruand@gmail.com>
2024-03-19 19:10:50 +0200

    Customize columns in admin: search, filter, ordering, readonly

--- job_app/admin.py
+++ job_app/admin.py
@@ -4,6 +4,10 @@ from .models import Form
 
 class FormAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    ordering = ("first_name", "last_name")
+    readonly_fields = ("occupation", )
 
 
 admin.site.register(Form, FormAdmin)
