4ac606c5c084efe97bbc4fd82edb654e5374657d
gruand <gruand@gmail.com>
2024-03-19 11:25:50 +0200

    Send email after form submission with gmail app

--- job_app/views.py
+++ job_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -17,6 +18,10 @@ def index(request):
             # Create object in Model (store data in database table)
             Form.objects.create(first_name=first_name, last_name=last_name,
                                 email=email, date=date, occupation=occupation)
+
+            message_body = f"Hi {first_name} {last_name}. Your job application form was submitted."
+            email_message = EmailMessage("Form submission confirmation(django)", message_body, to=[email, 'gruand@gmail.com'])
+            email_message.send()
 
             messages.success(request, "Form submitted successfully!")
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Specify settings for sending emails with gmail app
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = "587"
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "gruandapp@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ["GMAIL_APP_GRUANDAPP_KEY"] # Get my gmail app pass from environment
