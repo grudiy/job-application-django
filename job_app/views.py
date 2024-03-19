e91558216816797d42c54f503483927988cb353b
gruand <gruand@gmail.com>
2024-03-19 10:59:30 +0200

    Create ApplicationForm class(model) in view,  imported this class, instantiate it and extracted received data from form instance

--- job_app/views.py
+++ job_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from forms import ApplicationForm
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            occupation = form.cleaned_data["occupation"]
+
     return render(request, "index.html")
